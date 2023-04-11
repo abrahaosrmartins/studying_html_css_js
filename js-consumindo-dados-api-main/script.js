b181a5233a0bb60a1d0c90a2dde5eab792b97526
Abrahão Martins <abrahao.martins@edu.ufes.br>
2023-04-11 20:59:21 -0300

    Consumindo e tratando dados de uma API

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,13 +1,31 @@
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('CEP inválido')
-        } else {
-            console.log(r)
-        }
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Busca finalizada'));
+async function buscaEndereco(cep) {
+    let mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = '';
 
-console.log(consultaCEP);
+    try {
+        let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        let consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw new Error('CEP não encontrado');
+        }
+
+        let cidade = document.getElementById('cidade');
+        let estado = document.getElementById('estado');
+        let logradouro = document.getElementById('endereco');
+        let bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        estado.value = consultaCEPConvertida.uf;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        bairro.value = consultaCEPConvertida.bairro;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CPF não encontrado</p>`;
+        console.log(erro);
+    }
+}
+
+let cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
