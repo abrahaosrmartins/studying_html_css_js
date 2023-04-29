6cdc0a8a4e6c47a0458b2b907fb93b7f6a999298
Abrahão Martins <abrahao.martins@edu.ufes.br>
2023-04-29 14:12:35 -0300

    validando formulários

--- monibank-main/js/valida-cpf.js
+++ monibank-main/js/valida-cpf.js
@@ -1,21 +1,23 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf);
-    validaPrimeiroDigito(cpf);
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        debugger
+        campo.setCustomValidity('Esse CPF não é válido');
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
     const numerosRepetidos = [
         '00000000000',
-    '11111111111',
-    '22222222222',
-    '33333333333',
-    '44444444444',
-    '55555555555',
-    '66666666666',
-    '77777777777',
-    '88888888888',
-    '99999999999'
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
     ];
 
     return numerosRepetidos.includes(cpf);
@@ -33,11 +35,32 @@ function validaPrimeiroDigito(cpf) {
         multiplicador--;
     }
 
-    soma = (soma*10) % 11;
+    soma = (soma * 10) % 11;
 
     if (soma == 10 || soma == 11) {
         soma = 0;
     }
 
     return soma != cpf[9];
+}
+
+/**
+ * função que valida o segundo dígito verificador do CPF
+ */
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
