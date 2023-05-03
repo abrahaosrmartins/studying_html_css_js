6f1a8221b3d31a4ca545818948de479d760016f4
Abrahão Martins <56268335+abrahaosrmartins@users.noreply.github.com>
2023-05-02 23:14:09 -0300

    Estudando jquery

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,30 +1,76 @@
-let frase = $(".frase").text();
-let numeroPalavras = frase.split(" ").length;
-let tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numeroPalavras);
+const tempoInicial = $("#tempo-digitacao").text();
+
+// $(document).ready(function () { //assim que todo o html é carregado, ele chama as funções
+// versão enxuta (apenas chama o dollar, abre parenteses e ja chama a função)
+$(function () {
+    atualizaTamanhoFrase();
+    incializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $("#botao-reiniciar").click(reiniciaJogo); // se a função não retorna nada eu mando sem parenteses?
+});
+
+function atualizaTamanhoFrase() {
+    const frase = $(".frase").text();
+    const numeroPalavras = frase.split(" ").length;
+    let tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numeroPalavras);
+}
 
 let campo = $(".campo-digitacao")
-campo.on('input', function () {
-    let conteudo = campo.val()
-    let quantidadePalavras = conteudo.split(/\S+/).length - 1;
-    let quantidadeCaracteres = conteudo.length
-    $("#contador-palavras").text(quantidadePalavras)
-    $("#contador-caracteres").text(quantidadeCaracteres)
-})
 
-let tempoRestante = $("#tempo-digitacao").text()
+function incializaContadores() {
+    campo.on('input', function () {
+        let conteudo = campo.val();
+        let quantidadePalavras = conteudo.split(/\S+/).length - 1;
+        let quantidadeCaracteres = conteudo.length;
+        $("#contador-palavras").text(quantidadePalavras);
+        $("#contador-caracteres").text(quantidadeCaracteres);
+    });
+}
 
-campo.one("focus", function () {
-    let cronometroId = setInterval(function () { //todo setInterval retorna o seu próprio ID
-        tempoRestante--
-        // console.log(tempoRestante);
-        $("#tempo-digitacao").text(tempoRestante)
-        if (tempoRestante < 1) {
-            campo.attr("disabled", true)
-            clearInterval(cronometroId)
+function inicializaCronometro() {
+    let tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function () {
+        let cronometroID = setInterval(function () { //todo setInterval retorna o seu próprio ID
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+                campo.toggleClass("campo-desativado");
+            }
+        }, 1000);
+    });
+}
+
+function inicializaMarcadores() {
+    let frase = $(".frase").text();
+    campo.on("input", function () {
+        let digitado = campo.val();
+        // let comparavel = frase.substr(0, digitado.length);
+        let digitouCorreto = frase.startsWith(digitado);
+        if(digitouCorreto) {
+         campo.addClass("borda-verde");
+        } else {
+         campo.addClass("borda-vermelha");
         }
-    }, 1000)
-})
+    });
+}
+
+function reiniciaJogo(){
+    campo.attr("disabled",false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+}
+
 
 /**
  * os métodos .text(), .val() e .attr() tem função de pegar ou modificar um atributo/texto/valor
