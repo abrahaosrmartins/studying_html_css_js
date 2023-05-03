adb98f6c079e84964b9bf2943163dc672f7a7897
Abrahão Martins <56268335+abrahaosrmartins@users.noreply.github.com>
2023-05-03 20:22:31 -0300

    estudando Jquery

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,80 +1,78 @@
-const tempoInicial = $("#tempo-digitacao").text();
+let tempoInicial = $("#tempo-digitacao").text();
+let campo = $(".campo-digitacao");
 
-// $(document).ready(function () { //assim que todo o html é carregado, ele chama as funções
-// versão enxuta (apenas chama o dollar, abre parenteses e ja chama a função)
-$(function () {
+$(function() {
     atualizaTamanhoFrase();
-    incializaContadores();
+    inicializaContadores();
     inicializaCronometro();
     inicializaMarcadores();
-    $("#botao-reiniciar").click(reiniciaJogo); // se a função não retorna nada eu mando sem parenteses?
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
 function atualizaTamanhoFrase() {
-    const frase = $(".frase").text();
-    const numeroPalavras = frase.split(" ").length;
+    let frase = $(".frase").text();
+    let numPalavras  = frase.split(" ").length;
     let tamanhoFrase = $("#tamanho-frase");
-    tamanhoFrase.text(numeroPalavras);
+
+    tamanhoFrase.text(numPalavras);
 }
 
-let campo = $(".campo-digitacao")
-
-function incializaContadores() {
-    campo.on('input', function () {
+function inicializaContadores() {
+    campo.on("input", function() {
         let conteudo = campo.val();
-        let quantidadePalavras = conteudo.split(/\S+/).length - 1;
-        let quantidadeCaracteres = conteudo.length;
-        $("#contador-palavras").text(quantidadePalavras);
-        $("#contador-caracteres").text(quantidadeCaracteres);
-    });
-}
 
-function inicializaCronometro() {
-    let tempoRestante = $("#tempo-digitacao").text();
-    campo.one("focus", function () {
-        let cronometroID = setInterval(function () { //todo setInterval retorna o seu próprio ID
-            tempoRestante--;
-            $("#tempo-digitacao").text(tempoRestante);
-            if (tempoRestante < 1) {
-                campo.attr("disabled", true);
-                clearInterval(cronometroID);
-                campo.toggleClass("campo-desativado");
-            }
-        }, 1000);
+        let qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        let qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
     });
 }
 
 function inicializaMarcadores() {
     let frase = $(".frase").text();
-    campo.on("input", function () {
+    campo.on("input", function() {
         let digitado = campo.val();
-        // let comparavel = frase.substr(0, digitado.length);
-        let digitouCorreto = frase.startsWith(digitado);
-        if(digitouCorreto) {
-         campo.addClass("borda-verde");
+        let comparavel = frase.substr(0, digitado.length);
+
+        if (digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
         } else {
-         campo.addClass("borda-vermelha");
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
         }
     });
 }
 
-function reiniciaJogo(){
-    campo.attr("disabled",false);
+function inicializaCronometro() {
+    let tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+    	let cronometroID = setInterval(function() {
+    		tempoRestante--;
+    		$("#tempo-digitacao").text(tempoRestante);
+    		if (tempoRestante < 1) {
+                clearInterval(cronometroID);
+                finalizaJogo();
+    		}
+    	}, 1000);
+    });
+}
+
+function finalizaJogo() {
+    campo.attr("disabled", true);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
     campo.val("");
-    $("#contador-palavras").text("0");
-    $("#contador-caracteres").text("0");
+    $("#contador-palavras").text(0);
+    $("#contador-caracteres").text(0);
     $("#tempo-digitacao").text(tempoInicial);
     inicializaCronometro();
     campo.toggleClass("campo-desativado");
-
     campo.removeClass("borda-vermelha");
     campo.removeClass("borda-verde");
 }
-
-
-/**
- * os métodos .text(), .val() e .attr() tem função de pegar ou modificar um atributo/texto/valor
- * o .text() é utilizado para tags de texto (h2, p, span...)
- * .val() é utilizado para campos do tipo input
- * .attr() é utilizado para capturar, modificar, adicionar ou remover atributos do HTML
- */
---
+++ alura-typer/public/js/placar.js
@@ -0,0 +1,34 @@
+function inserePlacar() {
+    let corpoTabela = $(".placar").find("tbody");
+    let usuario = "Abrahão"
+    let numPalavras = $("#contador-palavras").text();
+
+    let linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.append(linha);
+}
+
+function novaLinha(usuario, palavras) {
+    let linha = $("<tr>");
+    let colunaUsuario = $("<td>").text(usuario);
+    let colunaPalavras = $("<td>").text(palavras);
+    let colunaRemover = $("<td>");
+
+    let link = $("<a>").addClass("botao-remover").attr("href", "#");
+    let icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha() {
+    $(this).parent().parent().remove();
+}
